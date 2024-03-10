055458fb656866adddb293c8dbe3a342e36a4a76
Harshit-Jajoria <harshitjajoria66@gmail.com>
2024-03-10 11:13:36 +0530

    files

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send(`Hello from backend index.js and port number ${port}  Harshit Jajoria   `);
+  res.send(`Hello from backend index.js and port number ${port}  Harshit Jajori  `);
 });
 
 //routes
@@ -22,5 +22,5 @@ app.use('/api/v1', userRoutes);
 
 
 app.listen(port, () => {
-  console.log(`Server is running on port number ${port}  Harshit Jajoria `);
+  console.log(`Server is running on port number ${port}  Harshit Jajori `);
 });
