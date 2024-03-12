5c252a5f4ea5cdc6cfad4661e45651e49f494ac9
Harshit-Jajoria <122677014+Harshit-Jajoria@users.noreply.github.com>
2024-03-12 23:16:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send(`Hello from backend index.js and port number ${port}  Harshit Jajoria  `);
+  res.send(`Hello from backend index.js and port number ${port}  Harshit Jajoriaa  `);
 });
 
 //routes
